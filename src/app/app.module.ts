3275835b0c6356a341b8e6fa76f3d9c7420bf0d1
Victor Toma <victor.toma@cgm.com>
2021-08-13 11:26:02 +0300

    tour-of-heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   imports: [
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule,
+      HttpClientModule,
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+         dataEncapsulation: false,
+      }),
+   ],
+   declarations: [
+      AppComponent,
+      HeroesComponent,
+      HeroDetailComponent,
+      MessagesComponent,
+      DashboardComponent,
+   ],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
